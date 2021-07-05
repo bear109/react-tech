a422f8c8fa8a2d20d9c506b1ee6e259dac1606ca
bear109 <jokerbear109@gmail.com>
2021-07-05 21:46:15 +0900

    ch4,5 이벤트 핸들링/ ref

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 // import { Component } from 'react';
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
-import Say from './Say';
+// import Say from './Say';
+// import EventPratice from './EventPratice';
+/*import ValidationSample from './ValidationSample';
 import './App.css';
 
 import React from 'react';
 
 const App = () => {
-  return <Say />;
+  return <ValidationSample />;
 };
+
+export default App;*/
+
+import React, { Component } from 'react';
+// import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      </div>
+    );
+  }
+}
 
 export default App;
